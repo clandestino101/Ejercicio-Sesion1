1fc5baae4ad3c3fb88d35f40e6b8bfed47e9b6c4
clandestino101 <bragclandestino@gmail.com>
2021-12-16 20:13:37 +0100

    [Ejercicio Sesion1]

---
+++ src/com/sesion1/Ejercicio1.java
@@ -0,0 +1,36 @@
+package com.sesion1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // Tipo: primitivo, algunos no todos
+        int numEntero = 10;
+        System.out.println("primitivo tipo int: " + numEntero );
+
+        long numLargo = 10101010;
+        System.out.println("primitivo tipo long: " + numLargo);
+
+        double numDoble = 10.5;
+        System.out.println("primitivo tipo double: " + numDoble);
+
+        float numComaFlotante = 55.07f;
+        System.out.println("primitivo tipo float: " + numComaFlotante);
+
+        // Tipo: boleano, verdadero/falso
+        boolean num1 = true;
+        System.out.println("Tipo de dato: Verdadero, " + num1);
+
+        boolean num2 = false;
+        System.out.println("Tipo de dato: Falso, " +num2);
+
+        // Tipo: char
+        char caracter = 'A';
+        System.out.println("Tipo de dato char: " + caracter);
+
+        // Clase String predefinida de Java
+        String saludo = "Hola, OpenBootcamp";
+        System.out.println("Esta es una clase predefinida de Java, String: " + saludo);
+
+    }
+}
